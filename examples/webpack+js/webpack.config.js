6346d588a7cca005671b7d7eb19f5b067f8e806b
Henry <henry@azule.info>
2023-02-01 02:07:15 +0000

    add compression for all file types

--- examples/webpack+js/webpack.config.js
+++ examples/webpack+js/webpack.config.js
@@ -1,4 +1,6 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
 const path = require('path');
 
 module.exports = {
@@ -13,7 +15,24 @@ module.exports = {
       {
         patterns: ['index.html', 'styles.css'],
       }
-      )
+    ),
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
   ],
   experiments: {
     asyncWebAssembly: true,
